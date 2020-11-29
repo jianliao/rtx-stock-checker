d0c03292e85e772d89dcfe003265ab1f5c55598e
Jian Liao <jianliao@adobe.com>
2020-11-28 18:12:36 -0800

    more small tuning

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 
 const config = require('./config');
-
 const { Webhook } = require('discord-webhook-node');
-const hook = new Webhook(config.discord_webhook.url);
-const puppeteer = process.platform === 'linux' ? require('puppeteer-core') : require('puppeteer');
-const launchConfig = process.platform === 'linux' ? { headless: false, executablePath: '/usr/bin/chromium-browser' } : { headless: false }
 const winston = require('winston');
+const puppeteer = process.platform === 'linux' ? require('puppeteer-core') : require('puppeteer');
+const hook = new Webhook(config.discord_webhook.url);
+const launchConfig = process.platform === 'linux' ? { headless: false, executablePath: '/usr/bin/chromium-browser' } : { headless: false }
 
 const main = async () => {
   logger.log('info', 'Stock Status Checking -- Start');
+  console.time('Total');
   const browser = await puppeteer.launch(launchConfig);
   try {
     for (let site of config.sites) {
       logger.log('info', `  ${site.username} -- start`);
       const page = await browser.newPage();
+      if (site.device) {
+        const device = puppeteer.devices[site.device];
+        await page.emulate(device);
+      }
       try {
+        console.time(site.username);
         for (let product of site.products) {
           logger.log('info', `    ${product.name}`);
           await page.goto(product.url);
@@ -36,14 +41,20 @@ const main = async () => {
             setNotification(site.avatar, site.username, `${availableProduct.name}\n\n${availableProduct.status}\n\n${availableProduct.url}`);
           }
         }
+        console.timeEnd(site.username);
+      } catch(e) {
+        logger.log('error', e);
       } finally {
         page.close();
       }
     }
+  } catch(e) {
+    logger.log('error', e);
   } finally {
     browser.close();
     logger.log('info', 'Stock Status Checking -- End');
   }
+  console.timeEnd('Total');
   return Promise.resolve();
 };
 
@@ -71,5 +82,15 @@ const logger = winston.createLogger({
 logger.add(new winston.transports.Console({
   format: winston.format.simple(),
 }));
+
+const mobile = async () => {
+  const browser = await puppeteer.launch({headless: false});
+  const page = await browser.newPage();
+  const iPhone = puppeteer.devices['iPad'];
+  await page.emulate(iPhone);
+  await page.goto('https://www.adorama.com/l/?searchinfo=rtx%203070&sel=Item-Condition_New-Items');
+}
+
+// (async () => mobile())();
 
 (async () => main())();
